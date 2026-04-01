c249920f3c0ea7c035ae6a748dd0d7138d854d1f
lab151-23 <fillah23si@mahasiswa.pcr.ac.id>
2026-04-01 16:39:02 +0700

    pertemuan3

---
+++ app/src/main/java/com/example/ajikapps/pertemuan3/ThirdResaultActivity.kt
@@ -0,0 +1,21 @@
+package com.example.ajikapps.pertemuan3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ajikapps.R
+
+class ThirdResaultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_third_resault)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
